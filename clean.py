2a888b9df9f02a9e8ba8d79d7f1c0dbc706fc287
anonymous17022000 <50959697+anonymous17022000@users.noreply.github.com>
2020-09-10 08:46:49 -0500

    Update clean.py

--- clean.py
+++ clean.py
@@ -1,13 +1,22 @@
 for i in data:
   if not all(x.isalpha() or x.isspace() for x in i['word']):
     data.remove(i)
-  elif len(i['countrycode'])!=2 and i['countrycode'].isupper() or not l['countrycode'].isalpha():
+  elif len(i['countrycode'])!=2 or not i['countrycode'].isupper() or not i['countrycode'].isalpha():
     data.remove(i)
   elif not (i['recognized']==True or i['recognized']==False or i['recognized']=="true" or i['recognized']=="false"):
     data.remove(i)
-  elif len(i['key_id'])!=16:
+  elif len(i['key_id'])!=16 or not i['key_id'].isdigit():
     data.remove(i)
-  elif not all(len(x)==2 for x in i['drawing']):
+  elif not all(len(x)==2 for x in i['drawing']) or i['drawing']==[]:
     data.remove(i)
+  elif all(len(x)==2 for x in i['drawing']):
+    for j in i['drawing']:
+      lis=[len(x) for x in j]
+      if len(list(set(lis)))!=1:
+        data.remove(i)
+      else:
+        for k in j:
+          if not all(isinstance(item, int) for item in k):
+            data.remove(i)
   else:
     first_task(i)
