ca71777d744ee8ea5c70ea9b0b31960b430d3d1e
rishith <Rishith>
2020-09-26 20:32:17 +0530

    fixed task1 reducer

--- Assignment2/task1_reducer.py
+++ Assignment2/task1_reducer.py
@@ -4,6 +4,8 @@ nodes = dict()
 for line in sys.stdin:
   try:
     from_node,to_node = line.split("\t",1)
+    to_node = to_node.strip("\n")
+    to_node = to_node.strip("\r")
     if from_node not in nodes.keys():
       nodes[from_node] = list()
       nodes[from_node].append(to_node)
@@ -12,8 +14,9 @@ for line in sys.stdin:
   except Exception as e:
     # print("Task 2",line)
     continue
-
+f=open("v.txt","w")
 for key in sorted(nodes.keys()):
   print(key,sep=" ",end=" ")
   print(*nodes[key],sep=",")
-  
+  f.write("1\n")
+f.close()  
