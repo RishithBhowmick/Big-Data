9c6b5fba0348970dc1eaab4eed709fc176c8bea1
Rishith <brishi00@gmail.com>
2020-10-04 06:43:37 +0530

    Run on hadoop

--- Assignment2/task2_mapper.py
+++ Assignment2/task2_mapper.py
@@ -11,7 +11,6 @@ for i in f.readlines():
         page_rank = float(ele[1].strip("\n"))
         page_rank_dict[node] = page_rank
 
-
 for line in sys.stdin:
     try:
         ele = line.split(" ")
@@ -19,14 +18,16 @@ for line in sys.stdin:
         to_nodes = ele[1].strip("\n")
         to_nodes = to_nodes.split(",")
         num_nodes = len(to_nodes)    
-        node_contribution = page_rank_dict[node1]/num_nodes
+        node_contribution = float(page_rank_dict[node1]/num_nodes)
         
         #to handle node for contribution to itself
-        print(f"{node1}\t0") 
+        print(f"{node1}\t0") #this didnt add
         for node in to_nodes:
-                        
+                      
             #to handle the case if a node has no pagerank i.e. no outgoing links
-                if node in page_rank_dict.keys():
-                    print(f"{node}\t{node_contribution}")
+            if node in page_rank_dict.keys():
+                print(f"{node}\t{node_contribution}")
+                    
     except Exception as e:
-        continue
+        pass
+f.close()
--- Assignment2/task2_reducer.py
+++ Assignment2/task2_reducer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 import sys
-import functools
 
 page_rank_contribution = dict()
 for line in sys.stdin:
@@ -8,14 +7,13 @@ for line in sys.stdin:
     node,contrib = line.split("\t",1)
     contrib = float(contrib)
     if node not in page_rank_contribution.keys():
-      page_rank_contribution[node] = list()
-      page_rank_contribution[node].append(contrib)
+      page_rank_contribution[node] = contrib
     else:
-      page_rank_contribution[node].append(contrib)
+      page_rank_contribution[node]+=contrib
   except Exception as e:
-    print(e)
+    pass
 
 for key in sorted(page_rank_contribution.keys()):
-    pagerank = functools.reduce(lambda x,y:x+y,page_rank_contribution[key]) 
-    pagerank = 0.15000+(0.85000*pagerank)
-    print("%s,%.5f"%(key,round(pagerank,5))) 
+    pagerank = page_rank_contribution[key]
+    pagerank = 0.15+(0.85*round(pagerank,5))
+    print("%s,%.5f"%(key,pagerank)) 
