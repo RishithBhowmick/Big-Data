b01ddfccd64f5bd8852a85f82f73f1c67b453a14
Rishith Bhowmick <brishi00@gmail.com>
2020-11-07 13:34:05 +0530

    Added more command line args

--- Assignment3/task2.py
+++ Assignment3/task2.py
@@ -3,22 +3,21 @@ import pyspark
 import sys
 findspark.init()
 from pyspark.sql import SparkSession
-if len(sys.argv)==4:
+if len(sys.argv)==5:
 
     spark = SparkSession.builder.master("local[*]").getOrCreate()
-    shape = spark.read.option("header",True).csv(sys.argv[2])
+    shape = spark.read.option("header",True).csv(sys.argv[3])
     shape_stat = spark.read.option("header",True).csv(sys.argv[4])
-    from pyspark.sql.functions import *
     shape = shape.alias('shape')
     shape_stat = shape_stat.alias('shape_stat')
     s=shape.join(shape_stat, shape.key_id == shape_stat.key_id).select(shape["*"],shape_stat["timestamp"],shape_stat['recognized'],shape_stat['Total_Strokes'])
-    word = 'angel'
     k = 8
-    valoutput = s.filter((s.recognized=='False') & (s.Total_Strokes>k) & (s.word == word)).groupBy('countrycode').count().show()
+    valoutput = s.filter((s.recognized=='False') & (s.Total_Strokes<sys.argv[2]) & (s.word == sys.argv[1])).groupBy('countrycode').count().show()
     if not valoutput:
         print(0)
-    for i in valoutput.rdd.collect():
-        print(i['countrycode']+','+str(i['count']))
+    else:
+        for i in valoutput.rdd.collect():
+            print(i['countrycode']+','+str(i['count']))
 else:
     pass        
 
