1dda48ed8ffcb5ab625cdfeab0592fd6e2104000
Rishith Bhowmick <brishi00@gmail.com>
2020-11-07 13:28:33 +0530

    Added sys args to both files. Need to test

--- Assignment3/task1.py
+++ Assignment3/task1.py
@@ -3,19 +3,18 @@ import sys
 spark = pyspark.SparkContext()
 spark_session = pyspark.sql.SparkSession(spark)
 from pyspark.sql import functions as F
-print("Arguments length .......: ",len(sys.argv))
+# print("Arguments length .......: ",len(sys.argv))
 if len(sys.argv)==4:
 
 	df = spark_session.read.csv(sys.argv[3],inferSchema=True,header=True)
 
 	word_df = df[df['word']==sys.argv[1]]
-	#word_df.show()
 	avg_recognised_count = word_df.groupBy(word_df.recognized).agg(F.avg(word_df.Total_Strokes)).collect()
 
 	
 	if not avg_recognised_count:
-		print(0.00000)
-		print(0.00000)
+		print("{:.5f}".format(0))
+		print("{:.5f}".format(0))
 	#print(avg_recognised_count)
 	else:
 		print(round(avg_recognised_count[0]["avg(Total_Strokes)"],5))
@@ -31,3 +30,4 @@ else:
 #print("Word count is :",count_words)
 
 
+# print()
--- Assignment3/task2.py
+++ Assignment3/task2.py
@@ -1,15 +1,24 @@
 import findspark
 import pyspark
+import sys
 findspark.init()
 from pyspark.sql import SparkSession
+if len(sys.argv)==4:
 
-spark = SparkSession.builder.master("local[*]").getOrCreate()
-shape = spark.read.option("header",True).csv('/content/drive/My Drive/shape.csv')
-shape_stat = spark.read.option("header",True).csv('/content/drive/My Drive/shape_stat.csv')
-from pyspark.sql.functions import *
-shape = shape.alias('shape')
-shape_stat = shape_stat.alias('shape_stat')
-s=shape.join(shape_stat, shape.key_id == shape_stat.key_id).select(shape["*"],shape_stat["timestamp"],shape_stat['recognized'],shape_stat['Total_Strokes'])
-word = 'angel'
-k = 8
-valoutput = s.filter((s.recognized=='False') & (s.Total_Strokes>k) & (s.word == word)).groupBy('countrycode').count().show()
+    spark = SparkSession.builder.master("local[*]").getOrCreate()
+    shape = spark.read.option("header",True).csv(sys.argv[2])
+    shape_stat = spark.read.option("header",True).csv(sys.argv[4])
+    from pyspark.sql.functions import *
+    shape = shape.alias('shape')
+    shape_stat = shape_stat.alias('shape_stat')
+    s=shape.join(shape_stat, shape.key_id == shape_stat.key_id).select(shape["*"],shape_stat["timestamp"],shape_stat['recognized'],shape_stat['Total_Strokes'])
+    word = 'angel'
+    k = 8
+    valoutput = s.filter((s.recognized=='False') & (s.Total_Strokes>k) & (s.word == word)).groupBy('countrycode').count().show()
+    if not valoutput:
+        print(0)
+    for i in valoutput.rdd.collect():
+        print(i['countrycode']+','+str(i['count']))
+else:
+    pass        
+
